6057ec1bb322a38123eec9f708f3fa9b2a97a969
zhengzhen <827807932@qq.com>
2018-03-21 18:11:44 +0800

    歌手详情组件

--- music/src/App.vue
+++ music/src/App.vue
@@ -6,17 +6,20 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <Player></Player>
   </div>
 </template>
 
 <script>
   import Mheader from 'src/components/m-header/m-header'
   import Tab from 'src/components/tab/tab'
+  import Player from 'src/components/player/player'
 
   export default {
     components: {
       Tab,
-      Mheader
+      Mheader,
+      Player
     }
   }
 </script>
