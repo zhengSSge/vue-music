43353f08cc5691dc1ee11b5460154c89cc585d28
zhengzhen <827807932@qq.com>
2018-05-04 14:58:59 +0800

    新增下拉刷新

--- music/src/base/scroll/scroll.vue
+++ music/src/base/scroll/scroll.vue
@@ -1,5 +1,6 @@
 <template>
   <!--封装滑动-->
+  <!--wrapper是父容器，它会有固定的高度。content是父容器的第一个子元素，它的高度会随着内容的大小而撑高。那么，当 content 的高度不超过父容器的高度，是不能滚动的，而它一旦超过了父容器的高度，我们就可以滚动内容区了，这就是 better-scroll 的滚动原理。-->
   <div ref="wrapper">
     <slot></slot>
   </div>
@@ -59,13 +60,20 @@
             me.$emit('scroll', pos)
           })
         }
-        if (this.pullup) { // 上拉刷新
+        if (this.pullup) { // 是否派发滚动到底部事件，用于上拉加载
           this.scroll.on('scrollEnd', () => { // 滚动结束派发 scrollEnd 它只派发一次
             if (this.scroll.y <= (this.scroll.maxScrollY + 50)) { // 如果y 小于 maxScrollY 说明快到底部了
               this.$emit('scrollToEnd') // 然后派发事件
             }
           })
         }
+        /**
+         this.scroll.on('touchend', (pos) => { // 是否派发顶部下拉事件，用于下拉刷新
+          if (pos.y > 50) {
+            console.log('下拉刷新')
+          }
+        })
+         */
       },
 //      禁用 better-scroll
       disable () {
